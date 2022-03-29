bb76ac69ace214f96c57a85ec87c2c1a5877222e
RedShirt1142 <red.perc.shirt@icloud.com>
2022-03-29 12:18:06 +0900

    ログイン画面

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
                         <div class="row mb-3">
 
                             <div class="col-md-6 m-auto">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" placeholder="{{ __('メールアドレス') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="border-dark form-control @error('email') is-invalid @enderror" name="email" placeholder="{{ __('メールアドレス') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -26,7 +26,7 @@
                         <div class="row mb-3">
 
                             <div class="col-md-6 m-auto">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="{{ __('パスワード') }}" required autocomplete="current-password">
+                                <input id="password" type="password" class="border-dark form-control @error('password') is-invalid @enderror" name="password" placeholder="{{ __('パスワード') }}" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -36,29 +36,11 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
                         <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                            <div class="col-md-3 m-auto">
+                                <button type="submit" class="btn btn-light w-100 border-dark">
+                                    {{ __('ログイン') }}
                                 </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
                             </div>
                         </div>
                     </form>
